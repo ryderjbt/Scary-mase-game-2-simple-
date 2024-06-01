b8b885912dc62398a835901d1bb80a880e13fab7
ryder.jbt <ryder.jbt@outlook.com>
2024-06-01 20:02:03 +0100

    Basic point and click movement

---
+++ Unity/Game/Assets/ClickManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public void clickReact()
+    {
+        Debug.Log("test");
+    }
+}
